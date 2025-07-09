a63e2e881759adc9e9f70227d36a0141ea512d1d
rakib-utsho <rakibutsho1920@gmail.com>
2025-07-09 19:13:51 +0600

    set-up database

---
+++ Backend/db/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+function connectToDb() {
+  mongoose
+    .connect(
+      process.env.DB_CONNECT,
+      { useNewUrlParser: true, useUnifiedTopology: true },
+      () => {
+        console.log("Connected to DB");
+      }
+    )
+    .catch((err) => console.log(err));
+}
+
+module.exports = connectToDb;
