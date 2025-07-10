4a49afe3efee09bf36e7faa7556c2ce162b478ee
rakib-utsho <rakibutsho1920@gmail.com>
2025-07-10 18:20:15 +0600

    create user route

---
+++ Backend/services/user.service.js
@@ -0,0 +1,21 @@
+const userModel = require("../models/user.model");
+
+module.exports.createUser = async ({
+  firstName,
+  lastName,
+  email,
+  password,
+}) => {
+  if (!firstName || !email || !password) {
+    throw new Error("All fields are required");
+  }
+  const user = await userModel.create({
+    fullname: {
+      firstName,
+      lastName,
+    },
+    email,
+    password,
+  });
+  return user;
+};
