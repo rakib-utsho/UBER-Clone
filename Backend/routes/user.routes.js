079e05a5932f47b38f2e50e443cc3830e98b545b
rakib-utsho <rakibutsho1920@gmail.com>
2025-07-27 10:13:18 +0600

    I have created the profile route and its middleware for support

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { body } = require("express-validator");
 const userController = require("../controllers/user.controller");
+const authMiddleware = require("../middlewares/auth.middleware");
 
 router.post(
   "/register",
@@ -17,12 +18,18 @@ router.post(
   userController.registerUser
 );
 
-router.post("/login", [
-  body("email").isEmail().withMessage("Invalid Email"),
-  body
-    .apply("password")
-    .isLength({ min: 6 })
-    .withMessage("Password is incorrect"),
-], userController.loginUser);
+router.post(
+  "/login",
+  [
+    body("email").isEmail().withMessage("Invalid Email"),
+    body
+      .apply("password")
+      .isLength({ min: 6 })
+      .withMessage("Password is incorrect"),
+  ],
+  userController.loginUser
+);
+
+router.get("/profile",authMiddleware.authUser, userController.getUserProfile);
 
 module.exports = router;
